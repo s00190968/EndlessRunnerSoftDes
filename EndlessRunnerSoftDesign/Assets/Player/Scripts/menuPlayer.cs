bb820298dd989f1e88c137427b5842852ff0d3b4
Janessa <s00190968@mail.itsligo.ie>
2019-04-24 18:17:22 +0100

    animations working started with the main menu

--- EndlessRunnerSoftDesign/Assets/Player/Scripts/menuPlayer.cs
+++ EndlessRunnerSoftDesign/Assets/Player/Scripts/menuPlayer.cs
@@ -1,22 +1,12 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
-public class PlayerMovement : MonoBehaviour
+public class menuPlayer : MonoBehaviour
 {
     //Variables
     public float speed = 5;
     private float moveInput;
-
-    //if character can be controlled manually with keyboard on false it will go forward on it's own
-    public bool manualControlling = false;
-
-    public float jumpForce = 5;
-    public int maxJumps;//how many jumps can be done
-    int extraJumps;//how many are left
 
     private Rigidbody2D rb;
 
@@ -35,11 +25,6 @@ public class PlayerMovement : MonoBehaviour
     public LayerMask WhatIsGround;
 
 
-    void Awake()
-    {
-        extraJumps = maxJumps;
-    }
-
     void Start()
     {
         //rigid body
@@ -55,12 +40,6 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        #region don't worry about this
-        if (Input.GetKeyUp(KeyCode.F2))
-        {
-            ScreenCapture.CaptureScreenshot(DateTime.Now.ToString());
-        }
-        #endregion
 
         //change the direction the sprite is facing
         if (lastPosX < transform.position.x && !facingRight)//moves right but isn't facing right
@@ -74,22 +53,7 @@ public class PlayerMovement : MonoBehaviour
             flipSprite();
         }
 
-        //reset extra jumps
-        //check if was jumping but is now touching ground
-        if(Input.GetAxis("Jump") < 1 && extraJumps <= 0 && isGrounded)
-        {
-            extraJumps = maxJumps;
-        }
-
-        //for manual controlling
-        if (!manualControlling)
-        {
-            moveInput = .5f;
-        }
-        else
-        {
-            moveInput = Input.GetAxis("Horizontal");
-        }
+        moveInput = .5f;
 
         //update animations
         anim.SetFloat("speed", rb.velocity.x);
@@ -106,28 +70,8 @@ public class PlayerMovement : MonoBehaviour
 
         //ground checks
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, WhatIsGround);
-
-        //jumping
-        if (Input.GetAxis("Jump") > 0 && extraJumps > 0)
-        {
-            rb.velocity = Vector3.up * jumpForce;
-            extraJumps--;
-        }
-        else if (Input.GetAxis("Jump") > 0 && extraJumps == 0 && isGrounded)//can jump once if touching ground and extra jumps have been depleted
-        {
-            rb.velocity = Vector3.up * jumpForce;
-        }
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Enemy"))
-        {
-
-        }
-    }
-
-    //scale the object so that it's facing the other direction
     public void flipSprite()
     {
         if (!facingRight)
@@ -138,10 +82,5 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.localScale = new Vector3(startScale.x, startScale.y, startScale.z);
         }
-    }
-
-    public void Die()
-    {
-        SceneManager.LoadScene(0);
     }
 }
